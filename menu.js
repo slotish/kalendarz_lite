8649d3fa99c74b892f21b5fbfd85a938ec78971c
slotish <slotish@.gmail.com>
2016-09-20 22:21:59 +0200

    adding preView option no 1

--- menu.js
+++ menu.js
@@ -1,6 +1,9 @@
 
 
 $( document ).ready(function() {
+
+    appendPreview(1)
+
 	setTimeout(function(){
     $('.first_carousel').carousel({
     	interval: 3000,
@@ -21,6 +24,10 @@ $( document ).ready(function() {
         interval: 3000,
         pause: null
     });
+
+  
+    
+
     smoothScroll(1000);
 
     $('.firstCalendar').mouseover(function(){
@@ -56,6 +63,8 @@ $( document ).ready(function() {
         $('.fourthCalendar').css('border' , 'none');
     })
     
+
+    
 });
 
 function smoothScroll(speed) {
@@ -67,3 +76,18 @@ function smoothScroll(speed) {
     });
 }
 
+function appendPreview(n){
+    $('#lookUp').fadeIn("slow")
+    $('#lookUp').append(' <div id="Carousel" class="carousel  sixth_carousel carousel-fade center-block" data-ride="carousel"><div class="carousel-inner center-block " role="listbox"><div class="item active"><img src="images/'+n+'_1.png" alt="..." class="img-responsive center-block active"></div><div class="item"><img src="images/'+n+'_2.png" alt="..." class="img-responsive center-block"></div><div class="item"><img src="images/'+n+'_3.png" alt="..." class="img-responsive center-block"></div><div class="item"><img src="images/'+n+'_4.png" alt="..." class="img-responsive center-block"></div><div class="item"><img src="images/'+n+'_5.png" alt="..." class="img-responsive center-block"></div><div class="item"><img src="images/'+n+'_6.png" alt="..." class="img-responsive center-block"></div><div class="item"><img src="images/'+n+'_7.png" alt="..." class="img-responsive center-block"></div><div class="item"><img src="images/'+n+'_8.png" alt="..." class="img-responsive center-block"></div><div class="item"><img src="images/'+n+'_9.png" alt="..." class="img-responsive center-block"></div><div class="item"><img src="images/'+n+'_10.png" alt="..." class="img-responsive center-block"></div><div class="item"><img src="images/'+n+'_11.png" alt="..." class="img-responsive center-block"></div><div class="item"><img src="images/'+n+'_12.png" alt="..." class="img-responsive center-block"></div>');
+    $('#lookUp').append('</div>');
+    $('#lookUp').append('<a class="left carousel-control" href="#Carousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left">');
+    $('#lookUp').append('</span></a>');
+    $('#lookUp').append('<a class="right carousel-control" href="#Carousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right">');
+    $('#lookUp').append('</span></a>');
+    $('#lookUp').append('</div>');
+
+    $('.right').click(function(){
+        $('#lookUp').fadeOut("slow")
+    })
+
+}
